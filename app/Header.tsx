32c47f3b910625df4f68d690c6a4f96b737df78a
Liang <liang.du@sandbox365.jp>
2023-08-31 09:14:11 +0900

    temp: header

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,27 +1,45 @@
 import Link from 'next/link'
+import Image from 'next/image'
+
+const links: { name: string; href: string }[] = [
+  { name: 'Company', href: '/company' },
+  {
+    name: 'Service',
+    href: '/service'
+  },
+  {
+    name: 'Works',
+    href: '/works'
+  },
+  {
+    name: 'News',
+    href: '/news'
+  }
+]
 
 const Header = () => (
-  <div>
-    <ul className="flex gap-4 p-4">
-      <li>
-        <Link href={'/company'}>Company</Link>
-      </li>
-      <li>
-        <Link href={'/service'}>Service</Link>
-      </li>
-      <li>
-        <Link href={'/works'}>Works</Link>
-      </li>
-      <li>
-        <Link href={'/news'}>News</Link>
-      </li>
-      <li>
-        <Link href={'/contact'}>Contact</Link>
-      </li>
-      <li>
-        <Link href={'/vercel'}>Vercel</Link>
-      </li>
-    </ul>
+  <div className="border-b border-gray-800">
+    <div className="p-2 max-w-screen-xl mx-auto flex justify-between items-center ">
+      <div className="flex gap-[80px] items-center">
+        <div>
+          <Image src="/logo.png" alt="Mindox Logo" width={260} height={58} priority />
+        </div>
+        <ul className="flex gap-8">
+          {links.map(({ name, href }) => (
+            <li key={name}>
+              <Link href={href} className="p-6 header-link">
+                {name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <Link href={'/contact'} className="p-6 header-link">
+          Contact
+        </Link>
+      </div>
+    </div>
   </div>
 )
 
