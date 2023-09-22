55eb7de0dcba57c765c955a0c63f70de4d701a8b
Liang <liang.du@sandbox365.jp>
2023-09-22 12:18:46 +0900

    simply table

--- app/company/Profile.tsx
+++ app/company/Profile.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import { Table, TableBody, TableCell, TableContainer, TableRow } from '@mui/material'
 
 type DataType = {
   title: string
   subtitle: string
   items: {
     name: string
-    value: string | string[]
+    value: string
   }[]
 }
 
@@ -21,53 +20,35 @@ const data: DataType = {
     { name: '资本金', value: '300万円' },
     {
       name: '事業内容',
-      value: [
-        'コンピュータのソフトウェアの企画、研究、開発、設計、製造、販売、保守、リース並びにそれらに関するコンサルティング業務',
-        'インターネット等の通信ネットワーク、電子技術を利用した各種情報提供サービス及び情報収集サービス',
-        '前（各）号に附帯関連する一切の事業'
-      ]
+      value:
+        'コンピュータのソフトウェアの企画、研究、開発、設計、製造、販売、保守、リース並びにそれらに関するコンサルティング業務\n\nインターネット等の通信ネットワーク、電子技術を利用した各種情報提供サービス及び情報収集サービス\n\n前（各）号に附帯関連する一切の事業'
     }
   ]
 }
 
 const Profile = () => {
-  const createData = (name: string, value: string | string[]) => {
-    return { name, value }
-  }
-
   return (
     <div>
       <h1 className="text-center text-7xl font-bold">{data.title}</h1>
       <h2 className="mt-4 text-center text-lg">{data.subtitle}</h2>
-      <TableContainer className="bg-transparent mt-20">
-        <Table aria-label="Company Profile">
-          <TableBody>
-            {data.items.map((row) => {
-              const rowData = createData(row.name, row.value)
-              return (
-                <TableRow key={rowData.name}>
-                  <TableCell component="th" className="border-b border-gray-400 border-b-1">
-                    <div className="py-4 w-24 md:w-48 text-base">{rowData.name}</div>
-                  </TableCell>
-                  <TableCell>
-                    <div className="py-4 text-base">
-                      {Array.isArray(rowData.value) ? (
-                        <div className="flex flex-col gap-3">
-                          {rowData.value.map((item, index) => (
-                            <p key={index}>{item}</p>
-                          ))}
-                        </div>
-                      ) : (
-                        rowData.value
-                      )}
-                    </div>
-                  </TableCell>
-                </TableRow>
-              )
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <table className="table-auto bg-transparent mt-20">
+        <tbody>
+          {data.items.map((item, index) => (
+            <tr key={index} className="border-b border-[color-border-lighter] border-b-1">
+              <th className="py-10 border-b border-gray-400 font-normal text-left">
+                <div className="w-24 md:w-48 text-base ">{item.name}</div>
+              </th>
+              <td className="py-10">
+                <div className="text-base">
+                  {item.value.split('\n\n').map((value, index) => (
+                    <p key={index}>{value}</p>
+                  ))}
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
